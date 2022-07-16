a65bcda4dba713808144ad560ba072e4dc270aff
Tuesday <rahrah93@gmail.com>
2022-07-15 21:18:52 -0700

    adds projectile class

--- cat_about_town.py
+++ cat_about_town.py
@@ -84,6 +84,18 @@ class Player(object):
             win.blit(idle, (self.x, self.y))
             self.walk_count = 0
 
+class Projectile(object):
+    def __init__(self, x, y, radius, color, facing):
+        self.x = x
+        self.y = y
+        self.radius = radius
+        self.color = color
+        self.facing = facing
+        self.vel = 8 * facing
+
+    def draw(self, win):
+        pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
+
 
 # GEN. VARIABLES
 # x = 50
