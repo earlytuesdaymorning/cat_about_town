95d0a312e108a3bc1a1038caaab363a7c71f5036
Tuesday <rahrah93@gmail.com>
2022-07-18 14:46:42 -0700

    adds prey and their paths

--- cat_about_town.py
+++ cat_about_town.py
@@ -39,6 +39,8 @@ atk_r = pygame.image.load('assets/AttackR.png')
 # CLOCK/FPS
 clock = pygame.time.Clock()
 
+score = 0
+
 # OBJECT CLASSES
 class Player(object):
     def __init__(self, x, y, width, height):
@@ -65,33 +67,39 @@ class Player(object):
             self.walk_count = 0
         # reset once it becomes the number of images times 3... to be able to cycle them below
         
-        if self.idle:
-            win.blit(idle, (self.x, self.y))
+        if len(bullets) > 0 and self.left:
+            win.blit(atk_l, (self.x, self.y))
+        elif len(bullets) > 0 and self.right:
+            win.blit(atk_r, (self.x, self.y))
         else:
-            if not self.standing:
-                if self.left:
-                    if not self.is_jumping:
-                        self.vel = 5
-                        win.blit(walk_left[self.walk_count//3], (self.x, self.y))
-                        self.walk_count += 1
-                    elif self.is_jumping:
-                        self.vel = 10
-                        win.blit(jump_l, (self.x, self.y))
-                        self.walk_count += 1
-                elif self.right:
-                    if not self.is_jumping:
-                        self.vel = 5
-                        win.blit(walk_right[self.walk_count//3], (self.x, self.y))
-                        self.walk_count += 1
-                    elif self.is_jumping:
-                        self.vel = 10
-                        win.blit(jump_r, (self.x, self.y))
-                        self.walk_count += 1
+            if self.idle:
+                win.blit(idle, (self.x, self.y))
             else:
-                if self.right:
-                    win.blit(walk_right[5], (self.x, self.y))
+                if not self.standing:
+                    if self.left:
+                        if not self.is_jumping:
+                            self.vel = 5
+                            win.blit(walk_left[self.walk_count//3], (self.x, self.y))
+                            self.walk_count += 1
+                        elif self.is_jumping:
+                            self.vel = 10
+                            win.blit(jump_l, (self.x, self.y))
+                            self.walk_count += 1
+                    elif self.right:
+                        if not self.is_jumping:
+                            self.vel = 5
+                            win.blit(walk_right[self.walk_count//3], (self.x, self.y))
+                            self.walk_count += 1
+                        elif self.is_jumping:
+                            self.vel = 10
+                            win.blit(jump_r, (self.x, self.y))
+                            self.walk_count += 1
                 else:
-                    win.blit(walk_left[5], (self.x, self.y))
+                    if self.right:
+                        win.blit(walk_right[5], (self.x, self.y))
+                    else:
+                        win.blit(walk_left[5], (self.x, self.y))
+
         self.hitbox = (self.x + 10, self.y + 15, 50, 40)
         pygame.draw.rect(win, (255,0,0), self.hitbox, 2)
 
@@ -107,28 +115,18 @@ class Attack(object):
     def draw(self, win):
         pygame.draw.circle(win, self.color, (self.x, self.y), self.radius)
 
-class Enemy(object):
+class RedBird(object):
     fly_right = [
-        pygame.image.load('assets/R1.png'),
-        pygame.image.load('assets/R2.png'),
-        pygame.image.load('assets/R3.png'),
-        pygame.image.load('assets/R4.png'),
-        pygame.image.load('assets/R5.png'),
-        pygame.image.load('assets/R6.png'),
-        pygame.image.load('assets/R7.png'),
-        pygame.image.load('assets/R8.png'),
-        pygame.image.load('assets/R9.png')
+        pygame.image.load('assets/RBirdR1.png'),
+        pygame.image.load('assets/RBirdR2.png'),
+        pygame.image.load('assets/RBirdR3.png'),
+        pygame.image.load('assets/RBirdR4.png')
     ]
     fly_left = [
-        pygame.image.load('assets/L1.png'),
-        pygame.image.load('assets/L2.png'),
-        pygame.image.load('assets/L3.png'),
-        pygame.image.load('assets/L4.png'),
-        pygame.image.load('assets/L5.png'),
-        pygame.image.load('assets/L6.png'),
-        pygame.image.load('assets/L7.png'),
-        pygame.image.load('assets/L8.png'),
-        pygame.image.load('assets/L9.png')
+        pygame.image.load('assets/RBirdL1.png'),
+        pygame.image.load('assets/RBirdL2.png'),
+        pygame.image.load('assets/RBirdL3.png'),
+        pygame.image.load('assets/RBirdL4.png')
     ]
 
     def __init__(self, x, y, width, height, end):
@@ -145,7 +143,7 @@ class Enemy(object):
     
     def draw(self, win):
         self.move()
-        if self.walk_count + 1 >= 27:
+        if self.walk_count + 1 >= 12:
             self.walk_count = 0
         
         if self.vel > 0:
@@ -155,7 +153,7 @@ class Enemy(object):
             win.blit(self.fly_left[self.walk_count //3], (self.x, self.y))
             self.walk_count += 1
         
-        self.hitbox = (self.x + 20, self.y + 20, 30, 30)
+        self.hitbox = (self.x + 16, self.y + 18, 30, 30)
         pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
 
     def move(self):
@@ -174,20 +172,144 @@ class Enemy(object):
 
     def hit(self):
         print('hit')
-        pass
+
+
+class BrownBird(object):
+    fly_right = [
+        pygame.image.load('assets/BBirdR1.png'),
+        pygame.image.load('assets/BBirdR2.png'),
+        pygame.image.load('assets/BBirdR3.png'),
+        pygame.image.load('assets/BBirdR4.png')
+    ]
+    fly_left = [
+        pygame.image.load('assets/BBirdL1.png'),
+        pygame.image.load('assets/BBirdL2.png'),
+        pygame.image.load('assets/BBirdL3.png'),
+        pygame.image.load('assets/BBirdL4.png')
+    ]
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.end]
+        self.walk_count = 0
+        self.vel = 3
+        self.hitbox = (self.x + 20, self.y, 28, 60)
+        # ^ this is init, not how hitbox will be drawn
+
+    def draw(self, win):
+        self.move()
+        if self.walk_count + 1 >= 12:
+            self.walk_count = 0
+
+        if self.vel > 0:
+            win.blit(self.fly_right[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+        else:
+            win.blit(self.fly_left[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+
+        self.hitbox = (self.x + 16, self.y + 18, 30, 30)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+    def move(self):
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walk_count = 0
+        else:
+            if self.x - self.vel > self.path[0]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walk_count = 0
+
+    def hit(self):
+        print('hit')
+
+
+class Rat(object):
+    run_right = [
+        pygame.image.load('assets/RatR1.png'),
+        pygame.image.load('assets/RatR2.png'),
+        pygame.image.load('assets/RatR3.png'),
+        pygame.image.load('assets/RatR4.png'),
+        pygame.image.load('assets/RatR5.png')
+    ]
+    run_left = [
+        pygame.image.load('assets/RatL1.png'),
+        pygame.image.load('assets/RatL2.png'),
+        pygame.image.load('assets/RatL3.png'),
+        pygame.image.load('assets/RatL4.png'),
+        pygame.image.load('assets/RatL5.png')
+    ]
+
+    def __init__(self, x, y, width, height, end):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.end = end
+        self.path = [self.x, self.end]
+        self.walk_count = 0
+        self.vel = 3
+        self.hitbox = (self.x + 20, self.y, 28, 60)
+        # ^ this is init, not how hitbox will be drawn
+
+    def draw(self, win):
+        self.move()
+        if self.walk_count + 1 >= 15:
+            self.walk_count = 0
+
+        if self.vel > 0:
+            win.blit(self.run_right[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+        else:
+            win.blit(self.run_left[self.walk_count // 3], (self.x, self.y))
+            self.walk_count += 1
+
+        self.hitbox = (self.x + 16, self.y + 18, 30, 30)
+        pygame.draw.rect(win, (255, 0, 0), self.hitbox, 2)
+
+    def move(self):
+        if self.vel > 0:
+            if self.x + self.vel < self.path[1]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walk_count = 0
+        else:
+            if self.x - self.vel > self.path[0]:
+                self.x += self.vel
+            else:
+                self.vel = self.vel * -1
+                self.walk_count = 0
+
+    def hit(self):
+        print('hit')
+
 
 # DRAW FUNCTION
 def redraw_game_window():
     win.blit(bg, (0, 0))
     george.draw(win)
-    bird.draw(win)
+    red_bird.draw(win)
+    brown_bird.draw(win)
+    rat.draw(win)
     for bullet in bullets:
         bullet.draw(win)
     pygame.display.update()
 
 # MAIN LOOP
 george = Player(50, 525, 64, 64)
-bird = Enemy(200, 385, 64, 64, 1000)
+red_bird = RedBird(90, 185, 64, 64, 850)
+brown_bird = BrownBird(200, 385, 64, 64, 1080)
+rat = Rat(450, 535, 64, 64, 1030)
 atk_loop = 0 # our attack cool down
 bullets = []
 running = True
@@ -206,12 +328,20 @@ while running:
             running = False
 
     for bullet in bullets:
-        if bullet.y - bullet.radius < bird.hitbox[1] + bird.hitbox[3] and bullet.y + bullet.radius > bird.hitbox[1]:
-            if bullet.x + bullet.radius > bird.hitbox[0] and bullet.x - bullet.radius < bird.hitbox[0] + bird.hitbox[2]:
-                bird.hit
+        if bullet.y - bullet.radius < red_bird.hitbox[1] + red_bird.hitbox[3] and bullet.y + bullet.radius > red_bird.hitbox[1]:
+            if bullet.x + bullet.radius > red_bird.hitbox[0] and bullet.x - bullet.radius < red_bird.hitbox[0] + red_bird.hitbox[2]:
+                red_bird.hit
+                bullets.pop(bullets.index(bullet))
+        elif bullet.y - bullet.radius < brown_bird.hitbox[1] + brown_bird.hitbox[3] and bullet.y + bullet.radius > brown_bird.hitbox[1]:
+            if bullet.x + bullet.radius > brown_bird.hitbox[0] and bullet.x - bullet.radius < brown_bird.hitbox[0] + brown_bird.hitbox[2]:
+                brown_bird.hit
+                bullets.pop(bullets.index(bullet))
+        elif bullet.y - bullet.radius < rat.hitbox[1] + rat.hitbox[3] and bullet.y + bullet.radius > rat.hitbox[1]:
+            if bullet.x + bullet.radius > rat.hitbox[0] and bullet.x - bullet.radius < rat.hitbox[0] + rat.hitbox[2]:
+                rat.hit
                 bullets.pop(bullets.index(bullet))
 
-        if bullet.x < george.x + 74 and bullet.x > george.x - 16:
+        if bullet.x < george.x + 74 and bullet.x > george.x - 16 and bullet.y < george.y + 74 and bullet.y > george.y - 16:
             bullet.x += bullet.vel
         else:
             bullets.pop(bullets.index(bullet))
@@ -224,12 +354,12 @@ while running:
         else:
             facing = 1
 
-        if len(bullets) < 10:
+        if len(bullets) < 1:
             bullets.append(
                 Attack(
                     round(george.x + george.width //2), 
                     round(george.y + george.height //2), 
-                    6, 
+                    12, 
                     (0, 0, 0), 
                     facing
                 )
